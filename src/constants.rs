c9a166d2b8d25ccd2cb7d2e9762759473a69c333
Dioarya <dioaryaraditya@gmail.com>
2026-03-17 06:25:57 +0700

    refactor: Remove unused constants in src/constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,14 +1,7 @@
-use std::time::Duration;
-
 // --- Texture file paths (compiled in at build time) ---
 
 pub const BOARD_TEXTURE: &str = include_str!("board.txt");
 pub const DISK_TEXTURE: &str = include_str!("disk.txt");
-
-// --- Timing ---
-
-pub const TARGET_FPS: u64 = 60;
-pub const FRAME_DURATION: Duration = Duration::from_micros(1_000_000 / TARGET_FPS);
 
 // --- Game configuration ---
 
